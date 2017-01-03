efdeb3570729f5141b4526046aeed1447ec6d2b1
Matt Weiss <portnoy31191@gmail.com>
2017-01-02 22:29:38 -0500

    Changed to a django project. "Board" is showing up now, but I didn't get socket-io communication going yet.

---
+++ heartsgame/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HeartsgameConfig(AppConfig):
+    name = 'heartsgame'
---
+++ heartsgame/static/homemade/js/tile.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+class Tile {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+  
+};
+
+class Hand {
+	constructor(tiles) {
+		this.tiles = tiles;
+	}
+};
