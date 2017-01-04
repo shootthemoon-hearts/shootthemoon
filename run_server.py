6c7f337c1c5a8ff391f6e3b97999f032a1d9e17f
Matt Weiss <portnoy31191@gmail.com>
2017-01-04 18:22:51 -0500

    Made minor changes to run_server.py according to another post I found

--- run_server.py
+++ run_server.py
@@ -1,21 +1,28 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # This file was inspired by:
 # http://codysoyland.com/2011/feb/6/evented-django-part-one-socketio-and-gevent/
+# and 
+# http://www.pixeldonor.com/2014/jan/10/django-gevent-and-socketio/
 
 import os
 
 import django.core.handlers.wsgi
+import django.core.wsgi
 import django
-
-PORT=8000
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lessthan3s.settings")
-
-django.setup()
-application = django.core.handlers.wsgi.WSGIHandler()
-
 from socketio.server import SocketIOServer
 
-if __name__ == '__main__':
+def main():
+    PORT=8000
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lessthan3s.settings")
+
+    #django.setup()
+    #application = django.core.handlers.wsgi.WSGIHandler()
+    application = django.core.wsgi.get_wsgi_application()
+
+
+
     SocketIOServer(('', PORT), application, resource='socket.io').serve_forever()
 
+if __name__ == '__main__':
+    main()
