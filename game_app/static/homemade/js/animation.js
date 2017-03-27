345f64ea0b6c15b725b93af0ffbbf66e6a89a8bf
rook <HalfEllipsis@openaliasbox.org>
2017-03-26 21:51:50 -0400

    just starting out. need to switch branches though.

---
+++ game_app/static/homemade/js/animation.js
@@ -0,0 +1,11 @@
+
+function display_with_height(game,sprite,shadow_sprite,x,y,z,rotation){
+	
+}
+
+function animate_throw(){}
+function animate_in_parralel(){}
+function animate_in_series(){}
+
+
+'pass':{'cards':"" , 'id':id}
