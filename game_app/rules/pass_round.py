d281fb27072dd7a7498ca8579414e1da8e4139f1
Chameleorp <closedmailbox@openmailbox.org>
2017-04-02 14:09:10 -0400

    fixed small bug with trick logic regarding queen of spades check in trick turn.

--- game_app/rules/pass_round.py
+++ game_app/rules/pass_round.py
@@ -37,7 +37,7 @@ def send_delay_message(pr, player, turn_id):
         }
     }
     Channel('asgi.delay').send(delay_message)
-    
+
 def received_passed_cards(pr, player, passed_cards, turn_id):
     passed_cards_sorted = sorted(passed_cards)
     from_seat = player.position
