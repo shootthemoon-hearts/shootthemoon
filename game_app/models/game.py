16afed26569296a46a7b2878326df9fd42e6a42a
Matt Weiss <portnoy31191@gmail.com>
2017-04-09 21:47:12 -0400

    Changed game to use DateTimeField instead of DateField

--- game_app/models/game.py
+++ game_app/models/game.py
@@ -5,4 +5,4 @@ import datetime
 class Game(models.Model):
     active = models.BooleanField(default=False)
     group_channel = models.CharField(max_length=16,default='')
-    date = models.DateField(default=datetime.date.today)
+    date = models.DateTimeField(default=datetime.date.today)
