880f20b5a93edc9e98de19f86b1ba90a45ccb9e8
Matt Weiss <portnoy31191@gmail.com>
2017-07-16 02:41:03 -0400

    Improved Stats page Game History table
    Changes:
    1) Previously a string which would be displayed in the table was sent,
    now the server sends the time in POSIX standard time, and the browser
    decides how to display the data.
    2) There was a bug where it was double incrementing resulting in only
    half the data being shown
    3) It was using getDay() instead of getDate() on the javascript side
    resulting in incorrect dates being shown.

--- stats/static/homemade/js/game_history.js
+++ stats/static/homemade/js/game_history.js
@@ -1,35 +1,46 @@
-function createGameHistoryTable(game_history){
+// The data should come in formatted the same way as the table will look
+// The first row is the header
+// The rest is the actual data
+// The only formatting that really happens is that the date played comes in as
+// POSIX timestamp, so it needs to be converted to a human readable date
+function createGameHistoryTable(game_history) {
 
     var body = document.getElementById('stats');
-    var table  = document.createElement('table');
+    var table = document.createElement('table');
 
     // Get the header first
     var header = table.createTHead();
     var headerRow = header.insertRow(0);
 
-    // Get first item of history to see what the headers should be
-    var item0 = game_history[0];
-    var i = 0;
-    for (var key in item0) {
-    	var cell = headerRow.insertCell(i);
-    	cell.innerHTML = key;
-
-    	i++;
+    for (var i = 0; i < game_history[0].length; i++) {
+        cell = headerRow.insertCell();
+        cell.innerHTML = game_history[0][i];
     }
-    
+
     // Now that the headers are set up, let's do each row
-    for (var i = 0; i < game_history.length; i++) {
-    	// i + 1 since the header row already exists
-    	var row = table.insertRow(-1);
-    	var j = 0;
-    	for (var key in game_history[i]) {
-    		var cell = row.insertCell(j);
-    		cell.innerHTML = game_history[i][key];
-    		j++;
-    	}
-    	i++;
+    for (var i = 1; i < game_history.length; i++) {
+        var row = table.insertRow(i);
+        for (var j = 0; j < game_history[i].length; j++) {
+            var cell = row.insertCell(j);
+
+            value = game_history[i][j];
+
+            // If this is the date, it's a POSIX time stamp so format it
+            if (j == 0) {
+                // Server sends in seconds, javascript takes millis
+                // Conversion is necessary
+                var date = new Date(value * 1000);
+                // Add one to month since it are 0 indexed
+                var date_str = (date.getMonth() + 1) + "/" + date.getDate()
+                        + "/" + date.getFullYear();
+                value = date_str;
+            }
+
+            cell.innerHTML = value;
+        }
     }
 
     body.appendChild(table);
 }
+
 createGameHistoryTable(game_history);
