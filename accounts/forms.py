3dbada24c28139e2ee7b2026916fd9de59cfc3b1
Matt Weiss <portnoy31191@gmail.com>
2017-07-16 04:05:28 -0400

    Added validation on sign up
    Validation now checks for:
     - Already taken user names
     - Passwords less than 6 characters
    
    I also updated an error in the documentation.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django import forms
 
 MAX_USERNAME_LENGTH=100
+MIN_PASSWORD_LENGTH=6
 MAX_PASSWORD_LENGTH=50
 
 class LogInForm(forms.Form):
@@ -16,9 +18,9 @@ class LogInForm(forms.Form):
 
     def clean(self):
         '''This function validates the input the user enters when trying to
-        log in. The validation will not currently happen enter the user hits
-        the Submit button
-        
+        log in. The validation will not currently happen until the user hits
+        the Submit button.
+
         Raises:
             ValidationError: If the user authentication information the user
                 entered is incorrect.
@@ -39,3 +41,21 @@ class SignUpForm(forms.Form):
     password = forms.CharField(max_length=MAX_PASSWORD_LENGTH, 
                                widget=password_form)
 
+    def clean(self):
+        '''This function validates the input the user enters when trying to
+        sign up. The validation will not currently happen until the user hits
+        the Submit button
+
+        Raises:
+            ValidationError: If the user authentication information the user
+                entered is incorrect.
+        '''
+        cleaned_data = super(SignUpForm, self).clean()
+        username = cleaned_data['user_name']
+        password = cleaned_data['password']
+        users_with_same_name = User.objects.filter(username=username)
+        if len(users_with_same_name) is not 0:
+            raise forms.ValidationError("Username is already taken")
+        if len(password) < MIN_PASSWORD_LENGTH:
+            raise forms.ValidationError("Password must be at least %s characters" %
+                                        MIN_PASSWORD_LENGTH)
